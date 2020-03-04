8d9e31f2618ea7d95f76cd784c2e9d70ef467091
Gleb Zinoviev <gz0609@gmail.com>
2020-03-04 15:49:12 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let newConfig = [], checkStr = str;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    newConfig.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+
+  for (let j = 0; j < str.length; j+=2) {
+    for (let k = 0; k < newConfig.length; k++) {
+      checkStr = checkStr.replace(newConfig[k], "");  
+    }
+    if (checkStr.length == 0) {
+      return true;
+    }
+  } 
+
+  return false;
 }
